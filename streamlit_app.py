d97ad2165c3561914c363080cba6a2c2409f9194
ZayneLim <137024462+ZayneLim@users.noreply.github.com>
2023-06-19 17:31:11 +0800

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # converts into a tabular display using dataframes
 sl.dataframe(fruityvice_normalized)
 
-my_cnx = sc.connect(**streamlit.secrets["snowflake"])
+my_cnx = sc.connect(**sl.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
