6c7e3f79e4627bb406720c4106990fcda1eef88d
ZayneLim <137024462+ZayneLim@users.noreply.github.com>
2023-06-19 16:46:06 +0800

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,9 @@ sl.text(fruityvice_response.json()) # just writes the data tot he screen
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
+# converts into a tabular display using dataframes
+sl.dataframe(fruityvice_normalized)
 
 
 
